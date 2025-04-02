1a554847796b240b1f5cb79a8df7185cf95307d6
Pezza242 <periyearwood@gmail.com>
2025-04-02 22:17:57 +0100

    Added API integration in JS
    When you search a city it will show you the actual current temperature of that city.
    
    Also tweaked the positioning on the temperature unit in CSS

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  let temperatureData = response.data.temperature.current;
+  let currentTemperature = document.querySelector("#current-temperature");
+  //console.log(response.data);
+  currentTemperature.innerHTML = Math.round(temperatureData);
+}
+
+function searchCity(city) {
+  //let apiKey = "eb79bof31898546ffea432d4bb90t390";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=eb79bof31898546ffea432d4bb90t390&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function citySearchBar(event) {
   event.preventDefault();
   let searchBar = document.querySelector("#search-bar");
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = searchBar.value;
+  searchCity(searchBar.value);
 }
 
 let searchFormInput = document.querySelector("#search-form");
