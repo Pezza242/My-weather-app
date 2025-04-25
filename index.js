09661bf360864bfbb60f66913232436393d3f542
Pezza242 <periyearwood@gmail.com>
2025-04-25 16:21:04 +0100

    Updated CSS and JS
    Changed the layout inside the weather box and added the actual 'feels like' temperature of the searched city into the weather box

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function updateWeather(response) {
   let temperatureData = response.data.temperature.current;
   let currentTemperature = document.querySelector("#current-temperature");
-  //console.log(response.data);
+  console.log(response.data);
   currentTemperature.innerHTML = Math.round(temperatureData);
 
   let currentCity = document.querySelector("#current-city");
   let cityName = response.data.city;
   currentCity.innerHTML = cityName;
+
+  let feelsLike = document.querySelector("#feels-like");
+  let feelsLikeTemperature = response.data.temperature.feels_like;
+  feelsLike.innerHTML = `Feels like: ${Math.round(feelsLikeTemperature)}º`;
 }
 
 function searchCity(city) {
   //let apiKey = "eb79bof31898546ffea432d4bb90t390";
+  //Had to insert api key into the url instead because making a string for it kept showing 'Invalid api key'
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=eb79bof31898546ffea432d4bb90t390&units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
